5fb2e307e0dfe045a4e99f8a6a38e3c0adcc31d7
Pan-Maciek <koziejka.com@gmail.com>
2020-09-08 02:40:31 +0200

    react setup

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Canvas from './canvas'
+
+ReactDOM.render(<Canvas
+  width={innerWidth}
+  height={innerHeight}
+/>, document.getElementById('root'))
