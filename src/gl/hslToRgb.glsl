0e8dd8113ee13016381b901361f488a6a5344a46
Pan-Maciek <koziejka.com@gmail.com>
2020-09-14 22:38:42 +0200

    implemented hslToRgb

---
+++ src/gl/hslToRgb.glsl
@@ -0,0 +1,24 @@
+float hueToRgb(float p, float q, float t) {
+  if (t < 0.) t += 1.;
+  if (t > 1.) t -= 1.;
+  if (t < 1./6.) return p + (q - p) * 6. * t;
+  if (t < 1./2.) return q;
+  if (t < 2./3.) return p + (q - p) * (2./3. - t) * 6.;
+  return p;
+}
+
+vec4 hslToRgb(float h, float s, float l){
+  float r, g, b;
+
+  if (s == 0.) return vec4(l, l, l, 1);
+  float q = l < 0.5 ? l * (1. + s) : l + s - l * s;
+  float p = 2. * l - q;
+  return vec4(
+    hueToRgb(p, q, h + 1./3.),
+    hueToRgb(p, q, h),
+    hueToRgb(p, q, h - 1./3.),
+    1
+  );
+}
+
+#pragma glslify: export(hslToRgb)
