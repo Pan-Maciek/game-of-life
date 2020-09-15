144cf07a9066652a0bb89db815ff91f63f5d3a5f
Pan-Maciek <koziejka.com@gmail.com>
2020-09-15 19:40:39 +0200

    cleanup
    * spited up drawing, brush and step
    * updating framebufer and states on this.config (width|height) change

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -35,6 +35,13 @@ export default class Canvas extends Component<CanvasProps> {
   private renderStateProgramInfo: twgl.ProgramInfo
 
   private vm: Mat3 // view matrix
+  private fbi: twgl.FramebufferInfo
+  private previousState: WebGLTexture
+  private currentState: WebGLTexture
+  private bufferInfo: twgl.BufferInfo
+  private mc: MouseController
+  private brushSize: number
+  private hue: number
 
   constructor(props: Readonly<CanvasProps>) {
     super(props)
@@ -45,102 +52,115 @@ export default class Canvas extends Component<CanvasProps> {
     this.renderStateProgramInfo = twgl.createProgramInfo(this.gl, [renderStateVertexShader, renderStateFragmentShader])
     this.nextStateProgramInfo = twgl.createProgramInfo(this.gl, [nextStateVertexShader, nextStateFragmentShader])
     this.brushProgramInfo = twgl.createProgramInfo(this.gl, [brushVertexShader, brushFragmentShader])
-  }
 
-  componentDidUpdate() {
-    const rulesTexture = createRuleTexture(this.gl, this.props.rules ?? defaultRules)
-    this.vm.setAspectRatio(this.props.config.height / this.canvas.current.height * this.canvas.current.width / this.props.config.width)
+    this.componentDidUpdate()
 
-    this.gl.useProgram(this.nextStateProgramInfo.program)
-    twgl.setUniforms(this.nextStateProgramInfo, {
-      u_rules: rulesTexture,
-      u_size: [this.props.config.width, this.props.config.height]
-    })
-    this.gl.useProgram(this.brushProgramInfo.program)
-    twgl.setUniforms(this.brushProgramInfo, {
-      u_rules: rulesTexture,
-      u_size: [this.props.config.width, this.props.config.height]
-    })
-  }
-
-  componentDidMount() {
-    const canvas = this.canvas.current
-    const gl = this.gl = canvas.getContext('webgl')
-    let hue = random()
-
-    this.preparePrograms()
-
-    const initialCellWidth = 4
-    const initialScale = initialCellWidth * this.props.config.width / canvas.width
-    let brushSize = 10 / this.props.config.width
-
-    const bufferInfo = twgl.createBufferInfoFromArrays(gl, {
+    this.bufferInfo = twgl.createBufferInfoFromArrays(this.gl, {
       a_position: { numComponents: 2, data: [-1, -1, 1, -1, 1, 1, -1, 1] },
       a_texCoords: { numComponents: 2, data: [0, 0, 1, 0, 1, 1, 0, 1] }
     })
 
-    const createState = () => twgl.createTexture(gl, {
-      width: this.props.config.width, height: this.props.config.height,
-      mag: gl.NEAREST, min: gl.NEAREST,
-      wrap: gl.REPEAT
-    })
+    this.gl.useProgram(this.renderStateProgramInfo.program)
+    twgl.setBuffersAndAttributes(this.gl, this.renderStateProgramInfo, this.bufferInfo)
+  }
 
-    let currentState = createState()
-    let previousState = createState()
+  componentDidUpdate(oldProps?: Readonly<CanvasProps>) {
+    const rulesTexture = createRuleTexture(this.gl, this.props.rules ?? defaultRules)
+    this.vm.setAspectRatio(this.props.config.height / this.canvas.current.height * this.canvas.current.width / this.props.config.width)
 
-    gl.useProgram(this.renderStateProgramInfo.program)
-    twgl.setBuffersAndAttributes(gl, this.renderStateProgramInfo, bufferInfo)
+    if (this.props.config.width !== oldProps?.config?.width || this.props.config.height !== oldProps?.config?.height) {
+      this.fbi = twgl.createFramebufferInfo(this.gl, [], this.props.config.width, this.props.config.height)
 
-    this.vm = mat3.scale(initialScale)
-    this.componentDidUpdate()
+      const createState = () => twgl.createTexture(this.gl, {
+        width: this.props.config.width, height: this.props.config.height,
+        mag: this.gl.NEAREST, min: this.gl.NEAREST,
+        wrap: this.gl.REPEAT
+      })
 
-    const fbi = twgl.createFramebufferInfo(gl, [], this.props.config.width, this.props.config.height) // todo: must resize fb on config change
-    const step = () => {
-      gl.bindFramebuffer(gl.FRAMEBUFFER, fbi.framebuffer)
-      gl.viewport(0, 0, this.props.config.width, this.props.config.height)
-      gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, currentState, 0)
-
-      if (mc.draw) {
-        gl.useProgram(this.brushProgramInfo.program)
-        twgl.setUniforms(this.brushProgramInfo, {
-          u_radius: brushSize,
-          u_center: screenToTexture(mc.position),
-          u_previousState: previousState,
-          u_hue: hue
-        })
-      } else {
-        gl.useProgram(this.nextStateProgramInfo.program)
-        twgl.setUniforms(this.nextStateProgramInfo, { u_previousState: previousState })
-      }
-      twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLE_FAN)
+      this.currentState = createState()
+      this.previousState = createState()
     }
 
-    const draw = () => {
-      step()
-      gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-      gl.viewport(0, 0, canvas.width, canvas.height);
-      gl.useProgram(this.renderStateProgramInfo.program)
-      twgl.setUniforms(this.renderStateProgramInfo, {
-        u_currentState: currentState,
-        u_viewMatrix: this.vm
-      })
-      twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLE_FAN);
+    if (this.props.width !== oldProps?.width || this.props.height !== oldProps?.height) {
+      const u_size = [this.props.config.width, this.props.config.height]
 
-      [currentState, previousState] = [previousState, currentState]
+      this.gl.useProgram(this.nextStateProgramInfo.program)
+      twgl.setUniforms(this.nextStateProgramInfo, { u_rules: rulesTexture, u_size })
+
+      this.gl.useProgram(this.brushProgramInfo.program)
+      twgl.setUniforms(this.brushProgramInfo, { u_size })
+    }
+  }
+
+  screenToTexture({ x, y }) {
+    ({ x, y } = this.vm.inverse.vmul({ x, y }))
+    return [(x + 1) / 2, (y + 1) / 2]
+  }
+
+  private applyBrush() {
+    this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.fbi.framebuffer)
+    this.gl.viewport(0, 0, this.props.config.width, this.props.config.height)
+    this.gl.framebufferTexture2D(this.gl.FRAMEBUFFER, this.gl.COLOR_ATTACHMENT0, this.gl.TEXTURE_2D, this.currentState, 0)
+
+    this.gl.useProgram(this.brushProgramInfo.program)
+    twgl.setUniforms(this.brushProgramInfo, {
+      u_radius: this.brushSize,
+      u_center: this.screenToTexture(this.mc.position),
+      u_previousState: this.previousState,
+      u_hue: this.hue
+    })
+    twgl.drawBufferInfo(this.gl, this.bufferInfo, this.gl.TRIANGLE_FAN);
+    [this.currentState, this.previousState] = [this.previousState, this.currentState]
+  }
+
+  private step() {
+    this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.fbi.framebuffer)
+    this.gl.viewport(0, 0, this.props.config.width, this.props.config.height)
+    this.gl.framebufferTexture2D(this.gl.FRAMEBUFFER, this.gl.COLOR_ATTACHMENT0, this.gl.TEXTURE_2D, this.currentState, 0)
+
+    this.gl.useProgram(this.nextStateProgramInfo.program)
+    twgl.setUniforms(this.nextStateProgramInfo, {
+      u_previousState: this.previousState
+    })
+    twgl.drawBufferInfo(this.gl, this.bufferInfo, this.gl.TRIANGLE_FAN);
+    [this.currentState, this.previousState] = [this.previousState, this.currentState]
+  }
+
+  private draw() {
+    this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+    this.gl.viewport(0, 0, this.props.width, this.props.height);
+
+    this.gl.useProgram(this.renderStateProgramInfo.program)
+    twgl.setUniforms(this.renderStateProgramInfo, {
+      u_currentState: this.currentState,
+      u_viewMatrix: this.vm
+    })
+    twgl.drawBufferInfo(this.gl, this.bufferInfo, this.gl.TRIANGLE_FAN)
+  }
+
+  componentDidMount() {
+    const canvas = this.canvas.current
+    this.gl = canvas.getContext('webgl')
+
+    const initialCellWidth = 4
+    this.vm = mat3.scale(initialCellWidth * this.props.config.width / canvas.width)
+    this.brushSize = 10 / this.props.config.width
+
+    this.preparePrograms()
+
+    const draw = () => {
+      if (this.mc.draw) this.applyBrush()
+      this.step()
+      this.draw()
       requestAnimationFrame(draw)
     }
 
-    const screenToTexture = ({ x, y }) => {
-      ({ x, y } = this.vm.inverse.vmul({ x, y }))
-      return [(x + 1) / 2, (y + 1) / 2]
-    }
-
     const zoomIntensity = 0.2
-    const mc = new MouseController(canvas, {
+    this.mc = new MouseController(canvas, {
       drag: e => this.vm.translate(e.normalized_movement),
       zoom: e => this.vm.zoomInto(exp(sign(-e.deltaY) * zoomIntensity), e.normalized)
     })
-    window.addEventListener('mousedown', e => hue = random())
+    window.addEventListener('mousedown', e => this.hue = random())
 
     requestAnimationFrame(draw)
   }
