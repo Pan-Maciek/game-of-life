7d87fbf658b706f4f6b3e8ec0a3564d2b1fce01e
Pan-Maciek <koziejka.com@gmail.com>
2020-09-16 16:32:52 +0200

    added resising and pausing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Canvas from './canvas'
+import { Rule } from './rules'
 
-ReactDOM.render(<Canvas
-  width={innerWidth}
-  height={innerHeight}
-  config={{
-    width: 1024, 
-    height: 512
-  }}
-/>, document.getElementById('root'))
+class Main extends React.Component {
+  state = {
+    width: innerWidth,
+    height: innerHeight,
+    running: true,
+    size: 8192
+  }
+  componentDidMount() {
+    window.addEventListener('resize', e => {
+      this.setState({ width: innerWidth, height: innerHeight })
+    })
+    window.addEventListener('keydown', e => {
+      if (e.key == ' ') this.setState({ running: !this.state.running })
+    })
+  }
+  render() {
+    return <Canvas
+      width={this.state.width}
+      height={this.state.height}
+      config={{
+        width: this.state.size,
+        height: this.state.size
+      }}
+      running={this.state.running}
+      rules={[Rule.Die, Rule.Die, Rule.Keep, Rule.Spawn, Rule.Die, Rule.Die, Rule.Die, Rule.Keep, Rule.Die]}
+    />
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'))
